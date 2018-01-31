c69ce50395c6c560ff4e86091b3d00600bed97c9
parker lindley <fugazi8@gmail.com>
2018-01-30 20:45:36 -0700

    Add basic styling to cards

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -4,7 +4,16 @@ import './Card.css';
 const Card = () => {
   return (
     <article className='Card'> 
-      <h1>card</h1>
+      <header>
+        <h1 className="name">card</h1>
+        <i className="favorite">x</i>
+      </header>
+      <ul className="categories">
+        <li>Homeworld: tatooine</li>
+        <li>Species: human</li>
+        <li>Language: Galactic Basic</li>
+        <li>Population: 200000</li>
+      </ul> 
     </article>
   )
 }
